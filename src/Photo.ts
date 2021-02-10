1dad500746b4ace63a8a7e4d1c7f3c17e57367c0
Yvan Ross <Yvan.ross@gmail.com>
2021-02-09 20:08:12 -0500

    push

--- src/Photo.ts
+++ src/Photo.ts
@@ -5,10 +5,9 @@
 // https://www.npmjs.com/package/mime-types
 // https://www.npmjs.com/package/symlink-dir
 // https://www.npmjs.com/package/filelink
-
 // - Objectif Archiver les photos sur un disque externe et conserver les thumbnails sur l'ordinateur local
 // - archiver sous forme year/month/date/2001-12-01_10-32-59-nom-fichier.png
-// - pays/province/ville/rue
+// - pays/province/ville/rue => symlink
 // - conserver structure des répertoire d'origine avec de symlink
 
 var path = require('path');
@@ -20,35 +19,39 @@ export class Photo {
   private url: string;
   private year: string|undefined
   private month: string|undefined
-  private day:string|undefined
+  private day: string|undefined
+  private hour: string|undefined
+  private minute: string|undefined
+  private second: string|undefined
 
   constructor(url: string) {
+    console.log(url);
     this.url = url;
   }
-   
-  getCreationDate(): string|undefined {
-   let humanDateFormat: string|undefined = undefined;
+
+  private extract_exif_data(){
+    console.log()
     const buf = fs.readFileSync(this.url);
     const parser = ExifParserFactory.create(buf).parse();
 
     let tags = parser.tags;
+  
+    
     if(tags != undefined){
       let nbMillisecond:number = tags.DateTimeOriginal!;
-      // console.log(nbMillisecond);
       let date = new Date(nbMillisecond*1000);
-      // let options = {  year: 'numeric',  month: 'long', day: 'numeric' };
-      // humanDateFormat = date.toLocaleString("en-US",options) ;
        this.month = date.toLocaleString("en-US", {month: "2-digit"}) // December
        this.day = date.toLocaleString("en-US", {day: "numeric"}) // 9
        this.year = date.toLocaleString("en-US", {year: "numeric"}) // 2019
-      let hour = date.toLocaleString("en-US", {hour: "numeric",hour12: false,timeZone: 'UTC'}) // 10 AM
-      let min = date.toLocaleString("en-US", {minute: "numeric"}) // 30
-      let sec = date.toLocaleString("en-US", {second: "numeric"}) // 15
-      humanDateFormat = this.year+"-"+this.month+"-"+this.day+"_"+hour+"-"+min+"-"+sec;
-      // humanDateFormat = date.toLocaleString("en-US" ) ;
-      // console.log(humanDateFormat);//2019-12-9 10:30:15
-      console.log(humanDateFormat);
+       this.hour = date.toLocaleString("en-US", {hour: "numeric",hour12: false,timeZone: 'UTC'}) // 10 AM
+       this.minute = date.toLocaleString("en-US", {minute: "numeric"}) // 30
+      this.second = date.toLocaleString("en-US", {second: "numeric"}) // 15
     }
+  }
+
+  getCreationDate(): string|undefined {
+   let humanDateFormat: string|undefined = undefined;
+    humanDateFormat = this.year+"-"+this.month+"-"+this.day+"_"+this.hour+"-"+this.minute+"-"+this.second;
     return humanDateFormat?.toString();
   }
   getFilename():string{
@@ -64,13 +67,13 @@ export class Photo {
 
  
 
-  export(root:string): string{
-    let new_path = path.join(root,this.getDirectoryName());
+  archive(archive_path:string, thumbnail_path:string|undefined): string{
+    this.extract_exif_data();
+   
+    let new_path = path.join(archive_path,this.getDirectoryName());
     if (!fs.existsSync(new_path)) 
       fs.mkdirSync(new_path, { recursive: true }); 
 
-    console.log("XXXXXXXX");
-    console.log(new_path);
     fs.copyFile(this.url,path.join(new_path,this.getNewName()), callback);
     return new_path;
   }  
@@ -81,3 +84,5 @@ function callback(err:any) {
   if (err) throw err;
   // console.log('source.txt was copied to destination.txt');
 }
+
+
